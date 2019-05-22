952a5e1e79406b6a58798c2343bf355a3afa8967
undacat <shannon@ruder.net>
2019-05-22 11:39:59 -0700

    singleevent card update

--- client/src/pages/SingleEvent.js
+++ client/src/pages/SingleEvent.js
@@ -1,28 +1,41 @@
 import React from "react";
 import { ListGroup, Card, ListGroupItem } from 'react-bootstrap';
+import EventCard from '../components/EventCard/EventCard'
+import AuthService from '../components/AuthService'
+import axios from 'axios'
+const Auth = new AuthService();
+export default class SingleEvent extends React.Component {
 
-export default function SingleEvent(props) {
-  const artist = props.location.state.artist
-  const { image, name, address, venue_name, date, time } = artist
-  console.log(artist)
+
+  handleSave(event) {
+    event['email'] = Auth.getProfile().email
+    axios.post("/api/events/save", event).then(results=>{console.log(results)
+   })   
+  }
+
+  render () {
+    const artist = this.props.location.state.artist
+
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={image.url} />
-      <Card.Body>
-        <Card.Title>{name}</Card.Title>
-        <Card.Text>
-          {address}
-        </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroupItem>Venue: {venue_name}</ListGroupItem>
-        <ListGroupItem>Date: {date}</ListGroupItem>
-        <ListGroupItem>Time: {time}</ListGroupItem>
-      </ListGroup>
-      <Card.Body>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
-    </Card>
+    <EventCard event = {artist} handleSave={this.handleSave}/>
+    // <Card style={{ width: '18rem' }}>
+    //   <Card.Img variant="top" src={image.url} />
+    //   <Card.Body>
+    //     <Card.Title>{name}</Card.Title>
+    //     <Card.Text>
+    //       {address}
+    //     </Card.Text>
+    //   </Card.Body>
+    //   <ListGroup className="list-group-flush">
+    //     <ListGroupItem>Venue: {venue_name}</ListGroupItem>
+    //     <ListGroupItem>Date: {date}</ListGroupItem>
+    //     <ListGroupItem>Time: {time}</ListGroupItem>
+    //   </ListGroup>
+    //   <Card.Body>
+    //     <Card.Link href="#">Card Link</Card.Link>
+    //     <Card.Link href="#">Another Link</Card.Link>
+    //   </Card.Body>
+    // </Card>
   )
+}
 }
