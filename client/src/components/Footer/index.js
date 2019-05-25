85c37f3d09b7cc24ca8e3baa955ead44aa638f93
Ariana Melis <arianamelis@gmail.com>
2019-05-24 17:55:27 -0700

    jsonpackage

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,50 +1,28 @@
 import "./style.css";
 import React, { Component } from "react";
-import Modal from '../Modal';
+import Modal from "../Modal";
 import { Col, Row, Container } from "../Grid";
 
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <footer className="footer">
+          <div className="bottom">
+            <hr />
 
-    class Footer extends Component {
-        render() {
-            return (
-                <div>
-                <footer className='footer'>
-                    <div className='bottom'>
-                 
-                   <hr/>
-              
-                        
-                            <p className="center-align">
-                              
-                            <Modal />
-                   
-                       
-                                 </p>
-                                            
-                                 <p className="center-align">
-                   copyrights {    
-                        `IndiePlay 2019    `
-                    }
-                    </p>
-                        
-                                 <hr/>
-                    
-                        
-                             
-                                                        
-                        
+            <p className="center-align">
+              <Modal />
+            </p>
 
-                   
-                                                        
-                      
-                    </div>
-                </footer>
-                </div>
-            );
-        }
-    }
-    
+            <p className="center-align">copyrights {`IndiePlay 2019    `}</p>
 
-    
-    export default Footer;
-    
+            <hr />
+          </div>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default Footer;
